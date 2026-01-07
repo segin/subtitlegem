b3a489d0d48cccefe4f97f60aafbb5030bdef8f6
Kirn Gill II <segin2005@gmail.com>
2026-01-07 02:03:48 -0600

    ui: pass video properties to preview for compatibility check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1451,6 +1451,7 @@ export default function Home() {
                   currentTime={currentTime}
                   onTimeUpdate={setCurrentTime}
                   onDurationChange={setDuration}
+                  videoProperties={videoProperties}
                 />
             </div>
           </div>
